c9658ef269f0ec4c69ad5f3b519dcd199c089da6
Justin <justinconnors@hotmail.ca>
2023-03-11 16:10:12 -0400

    Readme sections are all complete, need to add links and license badges

--- index.js
+++ index.js
@@ -32,6 +32,22 @@ inquirer
             type: 'input',
             message: 'What would you like to add for tests to the README? Close the editor when you are done, ',
             name: 'readmeTests'
+        },
+        {
+            type: 'input;',
+            message: 'Enter your GitHub username',
+            name: 'readmeUsername'
+        },
+        {
+            type: 'input;',
+            message: 'Enter your contact Email address',
+            name: 'readmeEmail'
+        },
+        {
+            type: 'checkbox',
+            message: 'What license(s) would you like to add',
+            choices: ['MIT', 'Academic Free', 'Eclipse Public 1.0', 'Eclipse Public 2.0', 'ISC', 'Microsoft Public'],
+            name: 'readmeLicense'
         }
     ])
     .then((response) => {
@@ -41,9 +57,12 @@ inquirer
         const usage = response.readmeUsage;
         const cont = response.readmeCont;
         const tests = response.readmeTests;
+        const username = response.readmeUsername;
+        const email = response.readmeEmail;
+        const license = response.readmeLicense;
 
         fs.appendFile('newRead/README.md', 
-        `# ${title}\n## Description\n${desc}\n## Installation\n${readmeInst}\n## Usage\n${usage}\n## Contribution guidelines\n${cont}\n## Tests\n${tests}`, 
+        `# ${title}\n## Description\n${desc}\n## Installation\n${readmeInst}\n## Usage\n${usage}\n## Contribution guidelines\n${cont}\n## Tests\n${tests}\n## Questions!\nHere's my GitHub: ${username}\nHere's my Email: ${email}\n## License\n${license}`, 
         (err) => {
             if (err) {
                 console.log(err);
